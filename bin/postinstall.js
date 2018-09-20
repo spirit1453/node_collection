df1b18d0c9f425bd1e8cd5ea1bc27561c0b6b568
spirit <catchspirit37@gmail.com>
2018-09-20 14:49:19 +0800

    z - Thu Sep 20 2018 14:49:19 GMT+0800 (China Standard Time)

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,6 +1,6 @@
-const {ModuleUtil} = require('@ys/collection')
-const {installGit} = ModuleUtil
 const path = require('path')
 const rootPath = path.resolve(__dirname, '../')
+const ModuleUtil = require(path.resolve(rootPath, 'src/util/ModuleUtil'))
+const {installGit} = ModuleUtil
 
 installGit(rootPath)
