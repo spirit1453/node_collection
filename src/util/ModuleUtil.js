6d25b48b6a8c9263980d67cde165c3ab2a967f7a
spirit <catchspirit37@gmail.com>
2018-09-25 09:39:50 +0800

    z - Tue Sep 25 2018 09:39:50 GMT+0800 (China Standard Time)

--- src/util/ModuleUtil.js
+++ src/util/ModuleUtil.js
@@ -131,6 +131,9 @@ module.exports = result\n`
       if (!fs.existsSync(packageJsonPath)) {
         packageJsonPath = path.join(rootPathDir, '../', ...moduleNameAry, 'package.json')
       }
+      if (!fs.existsSync(packageJsonPath)) {
+        packageJsonPath = path.join(rootPathDir, '../../', ...moduleNameAry, 'package.json')
+      }
       const modulePackageObj = require(packageJsonPath)
       const sha = modulePackageObj._resolved.split('#').pop()
       result = sha
