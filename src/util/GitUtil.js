2d8900ea8557ae3dbb97a0cbf96d08c1815a8d4c
spirit <catchspirit37@gmail.com>
2018-09-28 09:54:45 +0800

    z - Fri Sep 28 2018 09:54:45 GMT+0800 (China Standard Time)

--- src/util/GitUtil.js
+++ src/util/GitUtil.js
@@ -1,8 +1,25 @@
+/* eslint-disable global-require */
 
-const gitUtil = {
-
-  deleteBr (param) {
+class gitUtil {
+  static deleteBr (param) {
     'sdfsdf'
+  }
+  static getCurrentBranch (rootFolder) {
+    return new Promise((resolve, reject) => {
+      const simpleGit = require('simple-git')(rootFolder)
+
+      simpleGit.branch((err, obj) => {
+        if (err) {
+          reject(err)
+        }
+        const {current, all} = obj
+        const result = {
+          name: current,
+          hasRemote: all.includes(`remotes/origin/${current}`)
+        }
+        resolve(result)
+      })
+    })
   }
 }
 
