d61496b2101c87a4f29142b00979ac594d7896de
spirit <catchspirit37@gmail.com>
2018-10-16 16:34:06 +0800

    z - Tue Oct 16 2018 16:34:06 GMT+0800 (CST)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const result = {
   CliUtil: require('./util/CliUtil'),
   ErrorUtil: require('./util/ErrorUtil'),
   FileUtil: require('./util/FileUtil'),
+  GitHubUtil: require('./util/GitHubUtil'),
   GitUtil: require('./util/GitUtil'),
   ModuleUtil: require('./util/ModuleUtil'),
   SSHUtil: require('./util/SSHUtil')
