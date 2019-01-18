5ec9ab2e39007e1de41c1d6e93877d3b54906673
spirit <catchspirit37@gmail.com>
2019-01-19 02:39:11 +0800

    z - Sat Jan 19 2019 02:39:10 GMT+0800 (China Standard Time)

--- bin/coverage.js
+++ bin/coverage.js
@@ -1,8 +1,9 @@
 const childProcess = require('child_process')
 const path = require('path')
+const fs = require('fs')
+
 const coverage = path.resolve(__dirname, '../local/coverage')
 const coverageIndex = path.resolve(coverage, 'lcov-report/index.html')
-const fs = require('fs')
 
 if (!fs.existsSync(coverage)) {
   childProcess.execSync(`npm run test`)
