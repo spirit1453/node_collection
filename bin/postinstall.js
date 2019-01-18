8fb79812bc8cfdef078a718146ab824bce516a4e
spirit <catchspirit37@gmail.com>
2019-01-19 02:47:31 +0800

    msg

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const rootPath = path.resolve(__dirname, '../')
+
 const ModuleUtil = require(path.resolve(rootPath, 'src/util/ModuleUtil'))
 const {installGit} = ModuleUtil
 
