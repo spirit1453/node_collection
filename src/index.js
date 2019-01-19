2168ce0d28ae36b503b333313797e428952ea85f
spirit <catchspirit37@gmail.com>
2019-01-19 23:09:50 +0800

    z - Sat Jan 19 2019 23:09:50 GMT+0800 (China Standard Time)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /* eslint-disable global-require */
 const result = {
   CliUtil: require('./util/CliUtil'),
@@ -10,14 +10,12 @@ const result = {
   ModuleUtil: require('./util/ModuleUtil'),
   SSHUtil: require('./util/SSHUtil')
 }
-=======
 
 const ModuleUtil = require('./util/ModuleUtil')
 const path = require('path')
 
 const rootPath = path.resolve(__dirname, '../')
 const result = ModuleUtil.requireAll(path.resolve(rootPath, 'src/util'))
->>>>>>> f58d9223f8d1aa612039eb44b788dd23da5f5c18
 
 Object.freeze(result)
 module.exports = result
