270a280c287829e59c56d43e1020a4ec73001276
catch <catchspirit37@gmail.com>
2019-06-19 16:30:11 +0800

    z - Wed Jun 19 2019 16:30:11 GMT+0800 (China Standard Time)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-
 /* eslint-disable global-require */
-const ModuleUtil = require('./util/ModuleUtil')
-const path = require('path')
-
-const rootPath = path.resolve(__dirname, '../')
-const result = ModuleUtil.requireAll(path.resolve(rootPath, 'src/util'))
+const result = {
+  CliUtil: require('./util/CliUtil'),
+  ElectronUtil: require('./util/ElectronUtil'),
+  ErrorUtil: require('./util/ErrorUtil'),
+  FileUtil: require('./util/FileUtil'),
+  GitHubUtil: require('./util/GitHubUtil'),
+  GitUtil: require('./util/GitUtil'),
+  ModuleUtil: require('./util/ModuleUtil'),
+  SSHUtil: require('./util/SSHUtil')
+}
 
 Object.freeze(result)
 module.exports = result
