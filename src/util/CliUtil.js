3fb00546b286902b7a8549577386317540646ff0
catch <catchspirit37@gmail.com>
2019-06-19 15:28:16 +0800

    z - Wed Jun 19 2019 15:28:16 GMT+0800 (China Standard Time)

--- src/util/CliUtil.js
+++ src/util/CliUtil.js
@@ -54,12 +54,7 @@ class CliUtil {
 
     return pid
   }
-  static execSync (cmd, option) {
-    const defaultOption = {
-      stdio: [process.stderr, process.stdin, process.stdout]
-    }
-    return childProcess.execSync(cmd, _.merge(defaultOption, option))
-  }
+
   static async mergeOption ({argv, option = {}}) {
     const questionAry = []
     const param = {}
@@ -84,6 +79,90 @@ class CliUtil {
       _.merge(param, answer)
     }
     return param
+  }
+
+  static execSync (cmd, option = {}) {
+    const defaultOption = {
+      stdio: [process.stderr, process.stdin, process.stdout]
+    }
+    info(`executing: ${cmd}`)
+    return childProcess.execSync(cmd, _.merge(defaultOption, option))
+  }
+
+  static exec(cmd, option = {}) {
+    return new Promise((resolve, reject) => {
+      info(`executing: ${cmd}`)
+
+      const cp = childProcess.exec(cmd, _.merge({}, option), (error, stdout, stderr) => {
+        const processError = (err) => {
+          console.error(err)
+          reject(err)
+        }
+        if (error) {
+          errorDebug(`${cmd} failed`)
+          processError(error)
+        }else{
+          // git output to stderr
+          if (stderr) {
+            warn(`${cmd} has stderr output`)
+            console.log(stderr)
+          }
+          console.log(stdout)
+          resolve()
+        }
+      })
+
+      cp.stdout.on('data', (d) => {
+        console.log(d)
+      })
+    })
+  }
+
+  static isInstalled(cmd, option={}) {
+    const {
+      alertInstalled = true,
+    } = option
+    let result = true
+    try {
+      let testCmd
+      if (os.platform() === 'win32') {
+        testCmd = `where ${cmd}`
+      } else {
+        testCmd = `which ${cmd}`
+      }
+      childProcess.execSync(testCmd)
+      if(alertInstalled) {
+        verbose(`${cmd} has already been installed`)
+      }
+    } catch(err) {
+      console.log(err.stderr.toString())
+      result = false
+    }
+    return result
+  }
+
+  static getCmdResult(cmd) {
+    return childProcess.execSync(cmd).toString().trim()
+  }
+
+  static printSepLine(title, option = {}) {
+    const {
+      sepCount = 20,
+      returnOnStart = false,
+      returnOnEnd = false
+    } = option
+    let info = ''
+    for(let i = 0; i < sepCount; i++ ) {
+      info += '*'
+    }
+    if (returnOnStart) {
+      console.log()
+    }
+    console.info(`${info}${title}${info}`)
+    if (returnOnEnd) {
+      console.log()
+    }
+
   }
 }
 
