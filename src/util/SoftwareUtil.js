f595627ba1f28fe4c045ccfffe1a49f84edc9c0a
spirit <catchspirit37@gami.com>
2019-11-20 13:32:48 +0800

    z - Wed Nov 20 2019 13:32:48 GMT+0800 (中国标准时间)

--- src/util/SoftwareUtil.js
+++ src/util/SoftwareUtil.js
@@ -5,6 +5,8 @@ class SoftwareUtil {
     let result
     if (SystemUtil.isMac()) {
       result = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    } else if (SystemUtil.isWindows()){
+      result = 'C:\\Program Files (x86)\\Google\\Chrome\\Application'
     } else {
       throw new Error(`not supported`)
     }
