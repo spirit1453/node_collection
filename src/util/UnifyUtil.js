56c424853c13409937251f7d4d5abc6e916ac165
catch <catchspirit37@gmail.com>
2020-01-10 13:18:46 +0800

    z - Fri Jan 10 2020 13:18:46 GMT+0800 (China Standard Time)

--- src/util/UnifyUtil.js
+++ src/util/UnifyUtil.js
@@ -9,16 +9,29 @@ class UnifyUtil {
     let cmd
     if (SystemUtil.isWindows()) {
       cmd = `start chrome.exe "${url}"`
+        if (shouldExecute) {
+            try {
+                 childProcess.execSync(cmd, {
+                              stdio: 'inherit'
+                            })
+            } catch(error) {
+                cmd = `start microsoft-edge:${url}`
+                childProcess.execSync(cmd, {
+                                          stdio: 'inherit'
+                                        })
+            }
+          }
     } else {
       cmd = `
           open "${url}"
         `
+          if (shouldExecute) {
+              childProcess.execSync(cmd, {
+                stdio: 'inherit'
+              })
+            }
     }
-    if (shouldExecute) {
-      childProcess.execSync(cmd, {
-        stdio: 'inherit'
-      })
-    }
+
 
     return cmd
   }
